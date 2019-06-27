1855321dd60976d0360cddb8ccbdbb135bf15211
Mario Capellari <49732622+mariocapellari@users.noreply.github.com>
2019-06-27 13:13:55 -0700

    example setup using max script and python scripts
    example setup using max script to call on python scripts.

---
+++ DVS_Setup.py
@@ -0,0 +1,14 @@
+print "Hello DVS_Setup.py"
+
+# **set custom plugin path**
+# pointing to a location on the vendor's machine
+# where our python and max scripts are being copied
+# to via SG Toolkit
+
+
+# **set up a new project**
+# this could be used to create a project based on the task/project
+# the vendor is working on
+# pathConfig.doProjectSetupSteps()
+
+# 
