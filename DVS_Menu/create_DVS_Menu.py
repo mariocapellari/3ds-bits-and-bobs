3efba6387739947969fe712712dde2a512101ce6
Mario Capellari <49732622+mariocapellari@users.noreply.github.com>
2019-09-01 00:24:11 -0700

    Create create_DVS_Menu.py

---
+++ DVS_Menu/create_DVS_Menu.py
@@ -0,0 +1,80 @@
+'''
+     Scaffolding to built out DVS Auto Loader for 3ds Max
+     
+     This will create a "DVS" Pulldown menu with a dummy action item entry
+'''
+import MaxPlus
+
+
+def outputMenuItem(item, recurse=True, indent=''):
+    text = item.GetTitle()
+    print indent, text if text else "----"
+    if item.HasSubMenu and recurse:
+        outputMenu(item.SubMenu, recurse, indent + '   ')
+
+
+def outputMenu(menu, recurse=True, indent=''):
+    for i in menu.Items:
+        outputMenuItem(i, recurse, indent)
+
+somethingHappened = False
+
+
+def doSomething():
+    global somethingHappened
+    somethingHappened = True
+    print 'Something happened'
+
+action = MaxPlus.ActionFactory.Create(
+    'Do something', 'Python demos', doSomething)
+
+
+def createTestMenu(name):
+    if not MaxPlus.MenuManager.MenuExists(name):
+        mb = MaxPlus.MenuBuilder(name)
+        if action._IsValidWrapper():
+            print "Created action"
+        else:
+            print "Failed to create action"
+        mb.AddItem(action)
+        mb.AddSeparator()
+        menu = mb.Create(MaxPlus.MenuManager.GetMainMenu())
+        print 'menu created', menu.Title
+    else:
+        print 'The menu ', name, ' already exists'
+
+
+def getLastMenuItem(menu=MaxPlus.MenuManager.GetMainMenu()):
+    return list(menu.Items)[-1]
+	
+
+
+def testLastItem(text):
+    assert(getLastMenuItem().Title == text)
+
+def main():
+    print "Seriously, I AM MAIN!!!"
+    
+    print "Removing any previously left 'menu items'"
+    MaxPlus.MenuManager.UnregisterMenu(u"DVS")
+    
+    print "Creating a new menu"
+    createTestMenu(u"DVS")
+    outputMenu(MaxPlus.MenuManager.GetMainMenu(), False)
+    testLastItem(u"DVS")
+
+    # assert(not somethingHappened)
+    # mi = getLastMenuItem()
+    # mi = list(mi.SubMenu.Items)[0]
+    # ai = mi.ActionItem
+    # ai.Execute()
+    # assert(somethingHappened)
+
+    # print "Unregistering the 'test' menu"
+    # MaxPlus.MenuManager.UnregisterMenu(u"Test")
+    # outputMenu(MaxPlus.MenuManager.GetMainMenu(), False)
+    # testLastItem(u"&Help")
+
+
+if __name__ == '__main__':
+    main()
